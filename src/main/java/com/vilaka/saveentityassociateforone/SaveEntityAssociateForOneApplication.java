ab17cf2c95ad4cabd8183f43eb1a4343ca8af9b3
pativilaka <p.vilaka@gmail.com>
2023-08-11 10:22:05 -0300

    First commit with configurations done

---
+++ src/main/java/com/vilaka/saveentityassociateforone/SaveEntityAssociateForOneApplication.java
@@ -0,0 +1,13 @@
+package com.vilaka.saveentityassociateforone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveEntityAssociateForOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveEntityAssociateForOneApplication.class, args);
+	}
+
+}
